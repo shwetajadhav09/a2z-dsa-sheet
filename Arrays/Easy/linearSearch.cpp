5bce6c75f8745fea36dd340077102a15c5c14c29
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-01-21 20:57:55 +0530

    added

---
+++ Arrays/Easy/linearSearch.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(int arr[], int n, int num){
+
+    for(int i=0; i<n ; i++){
+
+        if(arr[i] == num){
+            return 1;
+        }
+
+        
+    }
+
+    return -1;
+}
+
+int main(){
+
+   int arr[] = {1,2,3,4,5};
+   int num = 4;
+
+   int n = sizeof(arr)/sizeof(arr[0]);
+
+   int val= search(arr, n, num);
+
+   cout<< num ;
+    return 0;
+}
