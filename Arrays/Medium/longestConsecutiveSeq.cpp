70ef7720560c2a1b9727e9dd9f2598293841575d
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-02-08 22:17:02 +0530

    new file

---
+++ Arrays/Medium/longestConsecutiveSeq.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestSuccessiveElements(vector<int>&a) {
+    // Write your code here.
+    int n = a.size();
+    if(n == 0) return 0;
+
+    int longest = 1;
+
+    unordered_set<int> st;
+
+    for(int i=0; i<n; i++){
+        st.insert(a[i]);
+    }
+
+    for(auto it: st){
+        if(st.find(it - 1) == st.end()){
+            int count =1;
+            int x = it;
+
+            while(st.find(x + 1) != st.end()){
+                x=x+1;
+                count = count+ 1;
+            }
+            longest = max(longest, count);
+        }   
+    }
+
+    return longest;
+
+
+}
