c2822ae111a963338466d57191314d5178e5a25a
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-03-02 22:10:08 +0530

    new file

---
+++ Binary Search/minimumElement.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& nums) {
+        int n = nums.size();
+
+	int low = 0, high = n-1, ans = INT_MAX;
+
+	while(low<=high){
+		int mid = (low+high)/2;
+
+		if(nums[low]<=nums[mid]){
+
+			ans= min(ans, nums[low]);
+			low = mid+1;
+		}
+		else{
+			ans = min(ans, nums[mid]);
+			high = mid-1;
+		}
+	}
+
+	return ans;
+    }
