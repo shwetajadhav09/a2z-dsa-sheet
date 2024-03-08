358b09c83d528e0a2d981b131d82620b7341b2ba
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-03-08 15:42:02 +0530

    added to aggressiveCows.cpp

--- Binary Search/aggressiveCows.cpp
+++ Binary Search/aggressiveCows.cpp
@@ -1,5 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool canWePlace(vector<int> &stalls, int distance, int k){
+    int cowsCount = 1, last = stalls[0];
+    int n = stalls.size();
+    for(int i =1; i< n; i++){
 
+        if(stalls[i] - last >= distance ){
+            cowsCount++;
+            last = stalls[i];
+        }
+        if(cowsCount >= k) return true;
+    }
+    return false;
+
+    
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+    int n = stalls.size();
+     int low = 1, high = stalls[n-1] - stalls[0];
+     
+
+     while(low<=high){
+        int mid = (low+high)/2;
+         if(canWePlace(stalls, mid, k) == true){
+             low = mid+1;
+         }
+         else high = mid-1;
+     }
+
+     return high;
+
+     
+     
+     }
 
