72935047cd56d60ae19bf31093266a3a503dac6b
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-04-13 22:58:07 +0530

    new file

--- Linked List/DLL.cpp
+++ Linked List/DLL.cpp
@@ -34,4 +34,111 @@ Node* convertArrToDLL(vector<int> &arr){
         return head;
     }
 
+//delete head
+    Node* deleteHead(Node* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return nullptr; 
+    }
 
+    Node* prev = head;      
+   
+    head = head->next;    
+
+    head->back = nullptr;   
+
+    prev->next = nullptr;  
+
+    return head;          
+}
+
+//delete tail
+    Node * deleteTail(Node *head) {
+
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+
+    Node* tail = head;
+
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+
+    Node* previous = tail->back;
+    tail->back = nullptr;
+    previous->next = nullptr;
+
+    delete tail;
+
+    return head;
+}
+
+Node* removeKthElement(Node* head, int k){
+
+    int count =0;
+    Node* temp = head;
+
+    while(temp!=NULL){
+
+        count++;
+
+        if(count == k) break;
+
+        temp=temp->next;
+    }
+
+    Node* prev = temp->back;
+    Node* front = temp->next;
+
+    if(prev == NULL && front == NULL){
+        delete temp;
+        return NULL;
+    }
+
+    else if(prev == NULL){
+        deleteHead(head);
+
+    }
+
+    else if(front == NULL){
+        deleteTail(head);
+
+    }
+    prev->next = front;
+    front->back = prev;
+    temp->next = nullptr;
+    temp->back = nullptr;
+    delete temp;
+    return head;
+
+}
+
+
+//INSERTION
+
+Node* insertBeforeHead(Node* head, int val){
+    Node* newHead = new Node(val, head, nullptr);
+
+    head->back = newHead;
+    return newHead;
+}
+
+Node* insertBeforeTail(Node* head, int val){
+    if(head->next == NULL){
+        return insertBeforeHead(head, val);
+    }
+    Node* tail = head;
+
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+
+    Node* prev = tail->back;
+
+    Node* newNode = new Node(val, tail, prev);
+
+    prev->next = newNode;
+    tail->back = newNode;
+
+    return head;
+}
